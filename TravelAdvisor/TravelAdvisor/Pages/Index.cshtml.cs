e0bd5b9feda6e3513be260ac9b5128cd0d7baaa1
Praveena-ts <77545695+Praveena-ts@users.noreply.github.com>
2021-03-28 21:02:28 -0400

    Adding api key

--- TravelAdvisor/TravelAdvisor/Pages/Index.cshtml.cs
+++ TravelAdvisor/TravelAdvisor/Pages/Index.cshtml.cs
@@ -19,6 +19,9 @@ namespace TravelAdvisor.Pages
 
         public void OnGet()
         {
+            String key=System.IO.File.ReadAllText("CitiesAPIKey.txt");
+            String citiesdata= webClient.DownloadString("https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json" + key);
+
 
         }
     }
