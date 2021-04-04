3b3dfccc8439b349dfe8b7c4f16a16f1f8e4bf0b
ArjitaSingh <43373731+ArjitaSingh@users.noreply.github.com>
2021-04-04 14:37:33 -0400

    singh4aj_CodeReview3

--- TravelAdvisor/TravelAdvisor/Pages/Index.cshtml.cs
+++ TravelAdvisor/TravelAdvisor/Pages/Index.cshtml.cs
@@ -29,28 +29,32 @@ namespace TravelAdvisor.Pages
 
         public new string Url { get; set; }
 
-
+        public bool IsCityNull { get; set; }
         public void OnGet()
         {
 
             IsSearchCity = false;
         }
-       
+
         public void OnPost()
         {
             string city = BreweryCity;
-
-            Url = "https://api.openbrewerydb.org/breweries?by_city=" + city;
-
-
-            using (var webClient = new WebClient())
+            // Check whether city populated on screen
+            IsCityNull = string.IsNullOrEmpty(city);
+            // Fetch data from API only when city populated
+            if (!IsCityNull)
             {
-                string jsonString = webClient.DownloadString(Url);
-                Welcome[] welcome = Welcome.FromJson(jsonString);
-                ViewData["Welcome"] = welcome;
+                Url = "https://api.openbrewerydb.org/breweries?by_city=" + city;
+
+                using (var webClient = new WebClient())
+                {
+                    string jsonString = webClient.DownloadString(Url);
+                    Welcome[] welcome = Welcome.FromJson(jsonString);
+                    ViewData["Welcome"] = welcome;
+                }
+
+                IsSearchCity = true;
             }
-
-            IsSearchCity = true;
-
         }
-}}
+    }
+}
