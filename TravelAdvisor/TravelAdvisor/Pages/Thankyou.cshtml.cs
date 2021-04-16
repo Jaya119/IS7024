6f0b832c2cd6be36e9d906e1557dd205ce9fd243
Jaya119 <77543698+Jaya119@users.noreply.github.com>
2021-04-16 19:03:42 -0400

    UI changes

--- TravelAdvisor/TravelAdvisor/Pages/Thankyou.cshtml.cs
+++ TravelAdvisor/TravelAdvisor/Pages/Thankyou.cshtml.cs
@@ -1,22 +1,16 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Xml;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TravelAdvisor.Pages
 {
-    public class ContactModel : PageModel
+    public class ThankyouModel : PageModel
     {
-        
         public void OnGet()
         {
         }
-
     }
 }
